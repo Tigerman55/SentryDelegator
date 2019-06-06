6a6604e003fb43bb405acdff90b902230fee314c
Joseph Koch <joseph.koch55@gmail.com>
2019-06-06 14:59:25 -0400

    Suppress code style notice

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace SentryDelegator\Exception;
 
+// phpcs:disable
 class RuntimeException extends \RuntimeException implements ExceptionInterface
 {
 }
+// phpcs:enable
